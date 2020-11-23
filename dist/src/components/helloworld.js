dae3cbaf1ccc8efac7c7947d2e892e2c092aa25c
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-24 00:06:09 +0500

    App Updated

--- dist/src/components/helloworld.js
+++ dist/src/components/helloworld.js
@@ -10,7 +10,7 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Helloworld = function Helloworld() {
-  return /*#__PURE__*/_react.default.createElement("h1", null, "Hello World From Rooshan Ahmed");
+  return /*#__PURE__*/_react.default.createElement("h1", null, "Hello World From Rooshan Ahmed \xA9");
 };
 
 var _default = Helloworld;
--- src/components/helloworld.js
+++ src/components/helloworld.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Helloworld = () => (
-    <h1>Hello World From Rooshan Ahmed</h1>
+    <h1>Hello World From Rooshan Ahmed &copy;</h1>
 )
 
 export default Helloworld;
